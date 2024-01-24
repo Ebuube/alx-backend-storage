0f3e3658f3df7503db3c0bfc8a2091c038c2aeb4
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-23 19:13:12 -0800

    Task 12: Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Print log statistics"""
+from pymongo import MongoClient
+from pymongo.cursor import Cursor
+
+
+def count(cursor):
+    """
+    Return the number of elements in a pymongo cursor object
+    """
+    if type(cursor) is not Cursor:
+        return -1
+
+    size = (
+            cursor.explain()
+            .get('executionStats')
+            .get('executionStages')
+            .get('nReturned')
+        )
+    return size
+
+
+def log_stat():
+    """
+    Display statistics for a log database
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+    log_count = nginx.count_documents({})
+    get_count = count(nginx.find({'method': 'GET'}))
+    post_count = count(nginx.find({'method': 'POST'}))
+    put_count = count(nginx.find({'method': 'PUT'}))
+    patch_count = count(nginx.find({'method': 'PATCH'}))
+    del_count = count(nginx.find({'method': 'DELETE'}))
+    status_count = count(nginx.find({'path': '/status'}))
+
+    # Print details
+    print("{} logs".format(log_count))
+    print("Methods:")
+    print("\tmethod GET: {}".format(get_count))
+    print("\tmethod POST: {}".format(post_count))
+    print("\tmethod PUT: {}".format(put_count))
+    print("\tmethod PATCH: {}".format(patch_count))
+    print("\tmethod DELETE: {}".format(del_count))
+    print("{} status check".format(status_count))
+
+
+if __name__ == "__main__":
+    log_stat()
